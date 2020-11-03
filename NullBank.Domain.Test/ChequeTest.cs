9a4d0cce557d037d574ac02b4f7576747ce9d701
Erlon F Souza <erlon.efs@gmail.com>
2020-11-03 00:46:17 -0300

    Aula #3

---
+++ NullBank.Domain.Test/ChequeTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace NullBank.Domain.Test
+{
+	public class ChequeTest
+	{
+
+
+		[Theory]
+		[InlineData(1, "Um Real")]
+		[InlineData(2, "Dois Reais")]
+		[InlineData(3, "Três Reais")]
+		[InlineData(4, "Quatro Reais")]
+		[InlineData(5, "Cinco Reais")]
+		[InlineData(6, "Seis Reais")]
+		[InlineData(7, "Sete Reais")]
+		[InlineData(8, "Oito Reais")]
+		[InlineData(9, "Nove Reais")]
+		public void Quando_for__unidade__devera_constar_valores_corretamente(decimal valor, string valorPorExtenso)
+		{
+			var cheque = new Cheque(valor);
+
+			cheque.Valor.Should().Be(valor);
+			cheque.ValorPorExtenso.Should().Be(valorPorExtenso);
+
+		}
+
+		[Theory]
+		[InlineData(10, "Dez Reais")]
+		[InlineData(20, "Vinte Reais")]
+		[InlineData(30, "Trinta Reais")]
+		[InlineData(40, "Quarenta Reais")]
+		[InlineData(50, "Cinquenta Reais")]
+		[InlineData(60, "Sessenta Reais")]
+		[InlineData(70, "Setenta Reais")]
+		[InlineData(80, "Oitenta Reais")]
+		[InlineData(90, "Noventa Reais")]
+		public void Quando_for__dezena__devera_constar_valores_corretamente(decimal valor, string valorPorExtenso)
+		{
+			var cheque = new Cheque(valor);
+
+			cheque.Valor.Should().Be(valor);
+			cheque.ValorPorExtenso.Should().Be(valorPorExtenso);
+
+		}
+	}
+}
---
+++ NullBank.Domain/Cheque.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NullBank.Domain
+{
+	public class Cheque
+	{
+		public decimal Valor { get; }
+		public string ValorPorExtenso { get; }
+
+		public Cheque(decimal valor)
+		{
+			Valor = valor;
+
+			switch (valor)
+			{
+				case 1:
+					ValorPorExtenso = "Um Real";
+					break;
+				case 2:
+					ValorPorExtenso = "Dois Reais";
+					break;
+				case 3:
+					ValorPorExtenso = "Três Reais";
+					break;
+				case 4:
+					ValorPorExtenso = "Quatro Reais";
+					break;
+				case 5:
+					ValorPorExtenso = "Cinco Reais";
+					break;
+				case 6:
+					ValorPorExtenso = "Seis Reais";
+					break;
+				case 7:
+					ValorPorExtenso = "Sete Reais";
+					break;
+				case 8:
+					ValorPorExtenso = "Oito Reais";
+					break;
+				case 9:
+					ValorPorExtenso = "Nove Reais";
+					break;
+				default:
+					break;
+			}
+
+		}
+
+	}
+}
