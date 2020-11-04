087b0c16f8c56d0482ca86932cb1a19feb59b7d1
Erlon F Souza <erlon.efs@gmail.com>
2020-11-04 00:29:44 -0300

    Aula #5

--- NullBank.Domain.Test/ChequeTest.cs
+++ NullBank.Domain.Test/ChequeTest.cs
@@ -6,8 +6,6 @@ namespace NullBank.Domain.Test
 {
 	public class ChequeTest
 	{
-
-
 		[Theory]
 		[InlineData(1, "Um Real")]
 		[InlineData(2, "Dois Reais")]
@@ -38,6 +36,34 @@ namespace NullBank.Domain.Test
 		[InlineData(80, "Oitenta Reais")]
 		[InlineData(90, "Noventa Reais")]
 		public void Quando_for__dezena__devera_constar_valores_corretamente(decimal valor, string valorPorExtenso)
+		{
+			var cheque = new Cheque(valor);
+
+			cheque.Valor.Should().Be(valor);
+			cheque.ValorPorExtenso.Should().Be(valorPorExtenso);
+
+		}
+
+		[Theory]
+		[InlineData(11, "Onze Reais")]
+		[InlineData(12, "Doze Reais")]
+		[InlineData(13, "Treze Reais")]
+		[InlineData(14, "Quatorze Reais")]
+		[InlineData(15, "Quinze Reais")]
+		[InlineData(16, "Dezesseis Reais")]
+		[InlineData(17, "Dezessete Reais")]
+		[InlineData(18, "Dezoito Reais")]
+		[InlineData(19, "Dezenove Reais")]
+		[InlineData(21, "Vinte e Um Reais")]
+		[InlineData(22, "Vinte e Dois Reais")]
+		[InlineData(23, "Vinte e Três Reais")]
+		[InlineData(24, "Vinte e Quatro Reais")]
+		[InlineData(25, "Vinte e Cinco Reais")]
+		[InlineData(26, "Vinte e Seis Reais")]
+		[InlineData(27, "Vinte e Sete Reais")]
+		[InlineData(28, "Vinte e Oito Reais")]
+		[InlineData(29, "Vinte e Nove Reais")]
+		public void Quando_for__dezena_e_unidade__devera_constar_valores_corretamente(decimal valor, string valorPorExtenso)
 		{
 			var cheque = new Cheque(valor);
 
--- NullBank.Domain/Cheque.cs
+++ NullBank.Domain/Cheque.cs
@@ -22,15 +22,29 @@ namespace NullBank.Domain
 				moeda = "Reais";
 			}
 
-			return $"{ObterDezena(valor)}{ObterUnidade(valor)} {moeda}";
+			var dezena = ObterDezena(valor);
+			var unidade = ObterUnidade(valor);
+
+			if (!string.IsNullOrWhiteSpace(dezena) && !string.IsNullOrWhiteSpace(unidade))
+			{
+				dezena += " e ";
+			}
+
+			return $"{dezena}{unidade} {moeda}";
 		}
 
 		private string ObterUnidade(decimal valor)
 		{
-			if (valor < 1) return string.Empty;
-			if (valor > 9) return string.Empty;
+			var text = valor.ToString().ToCharArray();
+			var unidade = Int32.Parse(text[text.Length - 1].ToString());
 
-			switch (valor)
+			if(text.Length > 1)
+			{
+				var dezena = Int32.Parse(text[text.Length - 2].ToString());
+				if (dezena == 1) return string.Empty;
+			}
+
+			switch (unidade)
 			{
 				case 1:
 					return "Um";
@@ -57,10 +71,15 @@ namespace NullBank.Domain
 
 		private string ObterDezena(decimal valor)
 		{
-			switch (valor / 10)
+			var text = valor.ToString().ToCharArray();
+			if (text.Length < 2) return string.Empty;
+
+			var dezena = Int32.Parse(text[text.Length - 2].ToString());
+
+			switch (dezena)
 			{
 				case 1:
-					return "Dez";
+					return ObterUnidadeDezena(valor);
 				case 2:
 					return "Vinte";
 				case 3:
@@ -80,6 +99,39 @@ namespace NullBank.Domain
 				default:
 					return string.Empty;
 			}
+		}
+
+		private string ObterUnidadeDezena(decimal valor)
+		{
+			var text = valor.ToString().ToCharArray();
+			var unidadeDezena = Int32.Parse(text[text.Length - 1].ToString());
+
+			switch (unidadeDezena)
+			{
+				case 0:
+					return "Dez";
+				case 1:
+					return "Onze";
+				case 2:
+					return "Doze";
+				case 3:
+					return "Treze";
+				case 4:
+					return "Quatorze";
+				case 5:
+					return "Quinze";
+				case 6:
+					return "Dezesseis";
+				case 7:
+					return "Dezessete";
+				case 8:
+					return "Dezoito";
+				case 9:
+					return "Dezenove";
+				default:
+					return string.Empty;
+			}
+
 		}
 
 	}
