4c80544d37b57a883a1c8044c3ac195a84ef61b4
Erlon F Souza <erlon.efs@gmail.com>
2020-12-19 11:01:52 -0300

    Aula #16

---
+++ NullBank.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using NullBank.Domain;
+using System;
+
+namespace NullBank.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (long valor = 1; valor <= 1000000000; valor++)
+			{
+				var cheque = new Cheque(valor);
+				Console.WriteLine(cheque.ValorPorExtenso);
+			}
+		}
+	}
+}
